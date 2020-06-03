ef4c6b0ded76795d12f71d0013cd089b5e97920c
MarkSmirnov13 <marksmirnov13@gmail.com>
2020-06-03 13:08:59 +0000

    Deploying to gh-pages from  @ aee0b7aad0eddc50a892a940e7c14168765dad09 🚀

---
+++ precache-manifest.091de369f30c9abdf837a50117eaf752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e88295f273229ffdd9d5395428e01c5",
+    "url": "/products-manager/index.html"
+  },
+  {
+    "revision": "8ef47767d3b7c3bbbdfa",
+    "url": "/products-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1cf37ba83965578ab483",
+    "url": "/products-manager/static/css/main.283eea1f.chunk.css"
+  },
+  {
+    "revision": "8ef47767d3b7c3bbbdfa",
+    "url": "/products-manager/static/js/2.27cf7406.chunk.js"
+  },
+  {
+    "revision": "1cf37ba83965578ab483",
+    "url": "/products-manager/static/js/main.0ac1193a.chunk.js"
+  },
+  {
+    "revision": "e285768826187f25c408",
+    "url": "/products-manager/static/js/runtime~main.5fd5e2d6.js"
+  }
+]);
